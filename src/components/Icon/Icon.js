8d20b1b22d5368fcd760fe76712283496ae5b775
Anja Siewert <anja.siewert@live.de>
2023-03-12 18:56:55 +0100

    add toggle star icon function

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import "./Icon.css";
 import { ReactComponent as StarFilled } from "./star-filled.svg";
 import { ReactComponent as Star } from "./star.svg";
 
-export default function Icon({ isFavorite }) {
+export default function Icon() {
+  const [isFavorite, setisFavorite] = useState(false);
   return (
     <>
-      <button className="icon-button" aria-label="favorite">
+      <button
+        className="icon-button"
+        aria-label="favorite"
+        onClick={() => setisFavorite(!isFavorite)}
+      >
         {isFavorite ? <StarFilled /> : <Star />}
       </button>
     </>
