76dd5cf7cff4d40e4dea3228bdbe225c24046493
Anja Siewert <anja.siewert@live.de>
2023-03-12 19:10:29 +0100

    add array of entries

--- src/components/EntriesSection/EntriesSection.js
+++ src/components/EntriesSection/EntriesSection.js
@@ -1,3 +1,4 @@
+import { Fragment } from "react";
 import Badge from "../Badge/Badge";
 import Divider from "../Divider/Divider";
 import Entry from "../Entry/Entry";
@@ -16,25 +17,48 @@ export default function EntriesSection() {
           Favorites <Badge>1</Badge>
         </Tab>
       </Tabs>
-      <div className="entries-section__entries">
-        <Entry
-          date="Feb 27, 2028"
-          motto="Thats life in the city"
-          notes="Si sine causa? quae fuerit causa, mox videro; interea hoc tenebo, si mihi. Et quidem se repellere, idque instituit docere sic omne animal, simul atque."
-        />
-        <Divider />
-        <Entry
-          date="Feb 26, 2028"
-          motto="Thats life in the city"
-          notes="Si sine causa? quae fuerit causa, mox videro; interea hoc tenebo, si mihi. Et quidem se repellere, idque instituit docere sic omne animal, simul atque."
-        />
-        <Divider />
-        <Entry
-          date="Feb 25, 2028"
-          motto="Thats life in the city"
-          notes="Si sine causa? quae fuerit causa, mox videro; interea hoc tenebo, si mihi. Et quidem se repellere, idque instituit docere sic omne animal, simul atque."
-        />
-      </div>
+      <Fragment key={entries.id}>
+        <div className="entries-section__entries">
+          {entries.map(({ id, date, motto, notes }, index) => {
+            return (
+              <p>
+                <Entry id={id} date={date} motto={motto} notes={notes} />
+                {index < entries.length - 1 && <Divider />}
+              </p>
+            );
+          })}
+        </div>
+      </Fragment>
     </section>
   );
 }
+
+const entries = [
+  {
+    id: 1000,
+    date: "Feb 5, 2025",
+    motto: "We are in a state of chaos",
+    notes:
+      "Today I learned about React State. It was fun! I can't wait to learn more.",
+  },
+  {
+    id: 999,
+    date: "Feb 4, 2025",
+    motto: "Props, Props, Props",
+    notes:
+      "Today I learned about React Props. Mad props to everyone who understands this!",
+  },
+  {
+    id: 998,
+    date: "Feb 3, 2025",
+    motto: "How to nest components online fast",
+    notes:
+      "Today I learned about React Components and how to nest them like a pro. Application design is so much fun!",
+  },
+  {
+    id: 997,
+    date: "Feb 2, 2025",
+    motto: "I'm a React Developer",
+    notes: "My React-ion when I learned about React: 😍",
+  },
+];
