079a3e52e706ef3d29129dc2379847d4a6a1f74d
Anja Siewert <anja.siewert@live.de>
2023-03-12 15:35:48 +0100

    adding more styles

--- src/components/EntryForm/EntryForm.js
+++ src/components/EntryForm/EntryForm.js
@@ -1,13 +1,19 @@
 import Button from "../Button/Button";
 import Input from "../Input/Input";
 import Textarea from "../Textarea/Textarea";
+import "./EntryForm.css";
 
 export default function EntryForm() {
   return (
     <>
-      <Input text="Motto" />
-      <Textarea text="Notes" />
-      <Button />
+      <form className="EntryForm">
+        <fieldset className="EntryForm__fieldset">
+          <h2 className="EntryForm__header">New Entry</h2>
+          <Input text="Motto" />
+          <Textarea text="Notes" />
+        </fieldset>
+        <Button />
+      </form>
     </>
   );
 }
